33d43b32baab7c0984cd249ff24e292bd765bef4
FoxEti <etifox9@gmail.com>
2024-10-10 19:27:18 +0300

    Update root-provider.tsx

--- src/app/root-provider.tsx
+++ src/app/root-provider.tsx
@@ -1,6 +1,15 @@
 'use client';
 
 import { Auth0Provider } from '@auth0/auth0-react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retryDelay: attemptIndex => Math.min(1000 * 2 ** attemptIndex, 20000)
+    }
+  }
+});
 
 const RootProvider = ({
   children
@@ -19,16 +28,18 @@ const RootProvider = ({
     // >
     //   {children}
     // </Auth0Provider>
-    <Auth0Provider
-      domain="dev-3lnbnd21i41bvlyu.us.auth0.com"
-      clientId="mth0c30xPs3nSneqm2ukTaee5zgf1THf"
-      authorizationParams={{
-        redirect_uri: 'https://frumhire.vercel.app/',
-        audience: 'https://Freelancer/api/'
-      }}
-    >
-      {children}
-    </Auth0Provider>
+    <QueryClientProvider client={queryClient}>
+      <Auth0Provider
+        domain="dev-3lnbnd21i41bvlyu.us.auth0.com"
+        clientId="mth0c30xPs3nSneqm2ukTaee5zgf1THf"
+        authorizationParams={{
+          redirect_uri: 'https://frumhire.vercel.app/',
+          audience: 'https://Freelancer/api/'
+        }}
+      >
+        {children}
+      </Auth0Provider>
+    </QueryClientProvider>
   );
 };
 
